059a6ff4a8a176df096f1bce7f01dafee390ae3f
arnoid <stratoangel@gmail.com>
2019-09-12 11:53:20 +0200

    Add logging-component

--- Components/src/commonMain/kotlin/location/BaseLocationFlowable.kt
+++ Components/src/commonMain/kotlin/location/BaseLocationFlowable.kt
@@ -2,24 +2,24 @@ package com.splendo.mpp.location
 
 import com.splendo.mpp.flow.BaseFlowable
 import com.splendo.mpp.location.Location.UnknownReason
+import com.splendo.mpp.util.debug
 
-open class BaseLocationFlowable: BaseFlowable<Location>() {
+open class BaseLocationFlowable : BaseFlowable<Location>() {
 
     private var lastLocation: Location.KnownLocation? = null
 
     suspend fun set(location: Location.KnownLocation) {
         lastLocation = location
         super.set(location)
-        println("location sent")
     }
 
     suspend fun setUnknownLocation(reason: UnknownReason = UnknownReason.NOT_CLEAR) {
         val location = lastLocation?.let {
             Location.UnknownLocationWithLastLocation(it, reason)
         } ?: Location.UnknownLocationWithNoLastLocation(reason)
-        println("Send to channel: $location")
+        debug("Send to channel: $location")
         set(location)
-        println("unknown location sent")
+        debug("unknown location sent")
     }
 
 }
---
+++ logging-component/src/commonMain/kotlin/Logger.kt
@@ -0,0 +1,33 @@
+package com.splendo.mpp.util
+
+import ru.pocketbyte.hydra.log.Logger
+
+interface Logger {
+
+    /**
+     * Writes log with provided level and tag.
+     * @param level Log level
+     * @param tag Tag of the log record. Nullable
+     * @param message Message to be written into log
+     */
+    fun log(level: LogLevel, tag: String?, message: String)
+
+    /**
+     * Writes exception log with provided level and tag.
+     * @param level Log level
+     * @param tag Tag of the log record. Nullable
+     * @param exception Exception to be written into log
+     */
+    fun log(level: LogLevel, tag: String?, exception: Throwable)
+
+    companion object {
+        fun getLoggingComponentLogLevel(logLevel: ru.pocketbyte.hydra.log.LogLevel): LogLevel {
+            return when (logLevel) {
+                ru.pocketbyte.hydra.log.LogLevel.DEBUG -> LogLevel.DEBUG
+                ru.pocketbyte.hydra.log.LogLevel.INFO -> LogLevel.INFO
+                ru.pocketbyte.hydra.log.LogLevel.WARNING -> LogLevel.WARNING
+                ru.pocketbyte.hydra.log.LogLevel.ERROR -> LogLevel.ERROR
+            }
+        }
+    }
+}
