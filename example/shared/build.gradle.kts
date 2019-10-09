190701cab883c025b1cb969dd34f38597948ad45
Tijl Houtbeckers <tijl@houtbecke.rs>
2019-10-09 17:17:12 +0200

    Use shared location printer for showing location (part 2)

--- example/shared/build.gradle.kts
+++ example/shared/build.gradle.kts
@@ -1,15 +1,60 @@
 plugins {
-    id 'com.android.library'
-    id 'org.jetbrains.kotlin.multiplatform'
+    kotlin("multiplatform")
+    id("jacoco")
+    id("maven-publish")
+    id("com.android.library")
 }
 
 repositories {
     mavenCentral()
     google()
     jcenter()
-    maven { url "https://dl.bintray.com/pocketbyte/hydra/" }
+    maven("https://dl.bintray.com/pocketbyte/hydra/")
 }
 
-apply from: "../../gradle/component.gradle"
+apply(from = "../../gradle/component.gradle")
 
-kor
+kotlin {
+    sourceSets {
+        getByName("iosMain") {
+            val ext =  (gradle as ExtensionAware).extra
+
+            println("properties ${ext.properties}")
+
+            val singleSet = ext["ios_one_sourceset"] as Boolean
+            var iosArch = ext["ios_arch"]
+            val orgArch = iosArch
+
+            if (singleSet)
+                iosArch = "ios"
+
+            dependencies {
+
+                if (!(ext["exampleAsRoot"] as Boolean)) {
+                    implementation(project(":Components", "${iosArch}Default"))
+                } else {
+                    val libraryVersion = ext["library_version"]
+                    implementation("com.splendo.kaluga:Components-$orgArch:$libraryVersion")
+                }
+
+            }
+        }
+        getByName("commonMain") {
+            val ext =  (gradle as ExtensionAware).extra
+
+            dependencies {
+
+                if (!(ext["exampleAsRoot"] as Boolean)) {
+                    implementation(project(":Components", ""))
+                    implementation(project(":logging", ""))
+                } else {
+                    val libraryVersion = ext["library_version"]
+                    implementation("com.splendo.kaluga:Components:$libraryVersion")
+                }
+
+            }
+        }
+    }
+}
+
+
