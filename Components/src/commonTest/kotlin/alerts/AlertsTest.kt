5ae853cc424c28cba06250048006f07230b1f5c3
Grigory Avdyushin <avdyushin.g@gmail.com>
2019-10-11 12:10:31 +0200

    Added Alert/Action test

---
+++ Components/src/commonTest/kotlin/alerts/AlertsTest.kt
@@ -0,0 +1,30 @@
+package com.splendo.kaluga.test
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+import com.splendo.kaluga.alerts.Alert
+
+class AlertsTest {
+
+    @Test
+    fun testAlertBaseInitialization() {
+
+        var handlerWasCalled = false
+        val handler = { handlerWasCalled = true }
+        val action = Alert.Action("action", Alert.Action.Style.DEFAULT, handler)
+        val alert = Alert("title", "message", listOf(action))
+
+        assertEquals(alert.title, "title")
+        assertEquals(alert.message, "message")
+        assertEquals(alert.actions.count(), 1)
+        assertEquals(alert.actions.first().title, "action")
+        assertEquals(alert.actions.first().style, Alert.Action.Style.DEFAULT)
+        assertEquals(alert.actions.first().handler, handler)
+
+        assertFalse(handlerWasCalled)
+        action.handler()
+        assertTrue(handlerWasCalled)
+    }
+}
