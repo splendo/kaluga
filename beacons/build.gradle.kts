5e4bfcda129749153badeda1005d35299e0937b3
Dmitrii Soldatov <dmitrii@splendo.com>
2019-11-19 16:26:20 +0100

    Updated beacons build config to use common component configuration.

--- beacons/build.gradle.kts
+++ beacons/build.gradle.kts
@@ -1,105 +1,44 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetPreset
 import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
 
 val ext =  (gradle as ExtensionAware).extra
 
+apply(from = "../gradle/publishable_component.gradle")
+
 group = "com.splendo.kaluga"
 version = ext["library_version"]!!
-
-repositories {
-    google()
-    jcenter()
-}
 
 plugins {
     id("jacoco")
     id("maven-publish")
     id("com.android.library")
-    id("org.jetbrains.kotlin.multiplatform")
+    kotlin("multiplatform")
 }
 
-android {
-
-    compileSdkVersion(28)
-
-    defaultConfig {
-        minSdkVersion(16)
-        targetSdkVersion(28)
-    }
-
-    sourceSets {
-        val main by getting {
-            manifest.srcFile("src/androidLibMain/AndroidManifest.xml")
-            res.srcDir("src/androidLibMain/resources")
-        }
-        val androidTest by getting {
-            manifest.srcFile("src/androidLibAndroidTest/AndroidManifest.xml")
-            java.srcDir("src/androidLibAndroidTest/kotlin")
-            res.srcDir("src/androidLibAndroidTest/res")
-        }
-    }
+dependencies {
+    implementation("org.altbeacon:android-beacon-library:2.16.3")
+    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.0.0")
 }
 
 kotlin {
+    for (target in targets.filterIsInstance<KotlinNativeTarget>()) {
+        target.compilations {
+                    val main by getting {
+                        val carthageBuildDir = "$projectDir/src/iosMain/native/Carthage/Build/iOS"
+                        cinterops(Action {
+                            val eddystone by creating {
+                                defFile("src/iosMain/native/eddystone.def")
+                                includeDirs("$carthageBuildDir/Eddystone.framework/Headers")
+                            }
+                        })
+                    }
 
-    data class IosTarget(val name: String, val preset: String, val id: String)
-
-    val iosTargets = listOf(
-        IosTarget("ios", "iosArm64", "ios-arm64"),
-        IosTarget("iosSim", "iosX64", "ios-x64")
-    )
-
-    android {
-        publishAllLibraryVariants()
-    }
-
-    for ((targetName, presetName, id) in iosTargets) {
-        targetFromPreset(presets.getByName<KotlinNativeTargetPreset>(presetName), targetName) {
-            compilations {
-                val main by getting {
-                    val carthageBuildDir = "$projectDir/src/iosMain/native/Carthage/Build/iOS"
-                    cinterops(Action {
-                        val eddystone by creating {
-                            defFile("src/iosMain/native/eddystone.def")
-                            includeDirs("$carthageBuildDir/Eddystone.framework/Headers")
-                            extraOpts("-verbose")
-                        }
-                    })
-                }
             }
-            mavenPublication {
-                artifactId = "${project.name}-$id"
-            }
+        target.mavenPublication {
+            artifactId = "${project.name}-${target.name}"
         }
     }
-
-    sourceSets {
-
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-            }
-        }
-
-        val androidMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib"))
-                implementation(kotlin("reflect"))
-
-                implementation("com.bugsnag:bugsnag-android:4.11.0")
-            }
-        }
-
-        val iosMain by getting {
-
-        }
-
-        val iosSimMain by getting {
-            dependsOn(iosMain)
-        }
-
-    }
-
 }
 
 // Create Carthage tasks
@@ -126,6 +65,3 @@ tasks.withType(CInteropProcess::class) {
 tasks.named<Delete>("clean") {
     delete(buildDir)
 }
-
-// Temporary workaround for https://youtrack.jetbrains.com/issue/KT-27170
-configurations.create("compileClasspath")
