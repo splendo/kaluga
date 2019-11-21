1b58843b6e67e9cf8b897e88c91a81796749fd17
Grigory Avdyushin <avdyushin.g@gmail.com>
2019-11-21 12:14:10 +0100

    Update build.gradle.kts

--- test-utils/build.gradle.kts
+++ test-utils/build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
             dependencies {
                 api(kotlin("test"))
                 api(kotlin("test-junit"))
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.0")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.2")
                 implementation(project(":base", ""))
                 implementation(project(":logging", ""))
             }
