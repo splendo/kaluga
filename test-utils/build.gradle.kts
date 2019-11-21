f01e396d42b7b12ac967e8ba8d9136ad89549a79
Grigory Avdyushin <avdyushin.g@gmail.com>
2019-11-21 13:24:25 +0100

    Use kotlinx_corountines_version

--- test-utils/build.gradle.kts
+++ test-utils/build.gradle.kts
@@ -11,6 +11,7 @@ apply(from = "../gradle/publishable_component.gradle")
 
 group = "com.splendo.kaluga"
 version = ext["library_version"]!!
+val kotlinx_corountines_version = ext["kotlinx_corountines_version"]!!
 
 repositories {
     maven("https://dl.bintray.com/pocketbyte/hydra/")
@@ -22,7 +23,7 @@ kotlin {
             dependencies {
                 api(kotlin("test"))
                 api(kotlin("test-junit"))
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.2")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinx_corountines_version")
                 implementation(project(":base", ""))
                 implementation(project(":logging", ""))
             }
