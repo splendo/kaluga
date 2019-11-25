4444ff028aeaa5a58188aa114d7ffcb71b49fa19
Grigory Avdyushin <avdyushin.g@gmail.com>
2019-11-25 11:06:42 +0100

    Update AlertsInterfaceTests.kt

--- alerts/src/iosTest/kotlin/AlertsInterfaceTests.kt
+++ alerts/src/iosTest/kotlin/AlertsInterfaceTests.kt
@@ -3,7 +3,7 @@ package com.splendo.kaluga.test
 import com.splendo.kaluga.alerts.AlertBuilder
 import platform.UIKit.UIViewController
 
-class AndroidAlertsInterfaceTests: AlertsInterfaceTests() {
+class IOSAlertsInterfaceTests: AlertsInterfaceTests() {
 
     override val builder get() = AlertBuilder(UIViewController(null, null))
 }
