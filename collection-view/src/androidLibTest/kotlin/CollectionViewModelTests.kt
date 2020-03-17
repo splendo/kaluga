8968903cb115b6cf60754c7083026dddb5966dd0
Grigory Avdyushin <avdyushin.g@gmail.com>
2020-03-17 16:51:01 +0100

    Update CollectionViewModelTests.kt

--- collection-view/src/androidLibTest/kotlin/CollectionViewModelTests.kt
+++ collection-view/src/androidLibTest/kotlin/CollectionViewModelTests.kt
@@ -1,3 +1,20 @@
+/*
+ Copyright 2020 Splendo Consulting B.V. The Netherlands
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.MutableLiveData
@@ -20,23 +37,6 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
-
-/*
- Copyright 2020 Splendo Consulting B.V. The Netherlands
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
- */
 
 class CollectionViewModelTests {
 
@@ -75,6 +75,7 @@ class CollectionViewModelTests {
                 offerWithDelay()
             }
         }
+
         private suspend fun offerWithDelay() {
             delay(1_000)
             debug("Offered!")
