68cc7c1dc1c223757ec234919d5eb2f6ec3de438
Grigory Avdyushin <avdyushin.g@gmail.com>
2020-03-19 09:33:09 +0100

    Convert build config to kotlin

---
+++ collection-view/build.gradle.kts
@@ -0,0 +1,62 @@
+/*
+ Copyright 2020 Splendo Consulting B.V. The Netherlands
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+
+plugins {
+    kotlin("multiplatform")
+    id("jacoco")
+    id("com.android.library")
+    id("maven-publish")
+    id("org.jlleitschuh.gradle.ktlint")
+}
+
+apply(from = "../gradle/publishable_component.gradle")
+
+val ext = (gradle as ExtensionAware).extra
+group = "com.splendo.kaluga"
+version = ext["library_version"]!!
+
+android {
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+}
+
+dependencies {
+    androidTestImplementation("androidx.appcompat:appcompat:1.1.0")
+    androidTestImplementation("androidx.fragment:fragment-ktx:1.1.0")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                val ext = (gradle as ExtensionAware).extra
+                implementation(project(":base", ""))
+                implementation(project(":logging", ""))
+                implementation("androidx.appcompat:appcompat:1.1.0")
+                implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0-alpha01")
+                implementation("co.touchlab:stately-common:${ext["stately_version"]}")
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation("org.mockito:mockito-core:3.1.0")
+            }
+        }
+    }
+}
