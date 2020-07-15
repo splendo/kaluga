e0418eab6f290aba145093de89eb5d018ae1e961
Gijs van Veen <daeda88@gmail.com>
2020-07-15 22:56:42 +0200

    Revert order

--- base/src/commonTest/kotlin/text/NumberFormatterTest.kt
+++ base/src/commonTest/kotlin/text/NumberFormatterTest.kt
@@ -99,11 +99,11 @@ class NumberFormatterTest : BaseTest() {
     @Test
     fun testFormatPercentage() {
         val formatters = createFormatters(NumberFormatStyle.Percentage(minFraction = 0U, maxFraction = 2U))
-        assertEquals("80%", formatters.nlFormatter.format(0.8))
-        assertEquals("80%", formatters.usFormatter.format(0.8))
-        assertEquals("200%", formatters.nlFormatter.format(2.0))
         assertEquals("200%", formatters.usFormatter.format(2.0))
+        assertEquals("200%", formatters.nlFormatter.format(2.0))
 
+        assertEquals("80%", formatters.usFormatter.format(0.8))
+        assertEquals("80%", formatters.nlFormatter.format(0.8))
 
         assertEquals("80.12%", formatters.usFormatter.format(0.801234))
         assertEquals("80,12%", formatters.nlFormatter.format(0.801234))
