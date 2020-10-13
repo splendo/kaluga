3eb86340eac7831c3150245e0d26fc5926bd0a67
Tijl Houtbeckers <tijl@houtbecke.rs>
2020-10-13 10:47:32 +0200

    more version upgrades for AS 4.1, Kotlin 1.4.10, kotlin-serialization 1.0 final

--- example/ios/Supporting Files/build.gradle.kts
+++ example/ios/Supporting Files/build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:$android_gradle_plugin_version")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
         classpath("com.google.gms:google-services:4.3.3")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:9.2.1")
         classpath("com.adarshr:gradle-test-logger-plugin:2.1.0")
