fe5a6a45e795ca1e39154eb4e0c131aea8d81678
Tijl Houtbeckers <tijl@houtbecke.rs>
2020-10-30 14:15:13 +0100

    Use a stable locale for Date tests.

--- base/src/commonTest/kotlin/utils/DateTest.kt
+++ base/src/commonTest/kotlin/utils/DateTest.kt
@@ -20,6 +20,7 @@ package com.splendo.kaluga.base.test.utils
 import com.splendo.kaluga.base.utils.Date
 import com.splendo.kaluga.base.utils.Locale
 import com.splendo.kaluga.base.utils.TimeZone
+import com.splendo.kaluga.base.utils.enUsPosix
 import com.splendo.kaluga.base.utils.nowUtc
 import com.splendo.kaluga.base.utils.plus
 import com.splendo.kaluga.base.utils.utc
@@ -31,7 +32,7 @@ class DateTest {
 
     @Test
     fun testEquality() {
-        val now = Date.now()
+        val now = Date.now(locale = Locale.enUsPosix)
         assertEquals(now, now.copy(), "copied Date should be equal")
 
         val nearEpoch = Date.epoch(1001)
@@ -42,38 +43,38 @@ class DateTest {
 
     @Test
     fun testUTCDate() {
-        val utcNow = Date.nowUtc()
+        val utcNow = Date.nowUtc(locale = Locale.enUsPosix)
         val epochNow = utcNow.millisecondSinceEpoch
-        val now = Date.epoch(epochNow, TimeZone.utc)
+        val now = Date.epoch(epochNow, TimeZone.utc, locale = Locale.enUsPosix)
         assertEquals(utcNow.millisecondSinceEpoch, now.millisecondSinceEpoch)
         assertEquals(utcNow, now)
     }
 
     @Test
     fun testCreateEpochDate() {
-        val someDay = Date.epoch().apply {
+        val someDay = Date.epoch(locale = Locale.enUsPosix).apply {
             year = 2020
             month = 5
             day = 12
             hour = 8
             minute = 45
         }
-        val epoch = Date.epoch()
+        val epoch = Date.epoch(locale = Locale.enUsPosix)
 
         assertTrue(epoch < someDay)
     }
 
     @Test
     fun testCreateNowDate() {
-        val now = Date.now()
-        val epoch = Date.epoch()
+        val now = Date.now(locale = Locale.enUsPosix)
+        val epoch = Date.epoch(locale = Locale.enUsPosix)
 
         assertTrue(now > epoch)
     }
 
     @Test
     fun testUpdateDate() {
-        val epoch = Date.epoch()
+        val epoch = Date.epoch(locale = Locale.enUsPosix)
         val isEarlierThanGMT = epoch.timeZone.offsetFromGMTAtDateInMilliseconds(epoch) < 0
         assertEquals(if (isEarlierThanGMT) 1969 else 1970, epoch.year)
         assertEquals(if (isEarlierThanGMT) 12 else 1, epoch.month)
@@ -84,7 +85,7 @@ class DateTest {
 
     @Test
     fun testGet() {
-        val someDay = Date.epoch(574695462750, TimeZone.utc)
+        val someDay = Date.epoch(574695462750, TimeZone.utc, locale = Locale.enUsPosix)
 
         assertEquals(1, someDay.era)
         assertEquals(1988, someDay.year)
