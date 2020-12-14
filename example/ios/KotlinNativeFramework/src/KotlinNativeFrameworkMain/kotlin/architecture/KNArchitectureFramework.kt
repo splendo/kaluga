0324f5c2c613206f2d66788d7a432308a04db6a9
corrado4eyes <corradoquattrocchi@hotmail.it>
2020-12-14 09:38:01 +0100

    Add browser types.

--- example/ios/KotlinNativeFramework/src/KotlinNativeFrameworkMain/kotlin/architecture/KNArchitectureFramework.kt
+++ example/ios/KotlinNativeFramework/src/KotlinNativeFrameworkMain/kotlin/architecture/KNArchitectureFramework.kt
@@ -102,7 +102,7 @@ class KNArchitectureFramework {
                     is InfoNavigation.Link -> NavigationSpec.Browser(
                         NSURL.URLWithString(
                             action.bundle!!.get(LinkSpecRow.LinkRow))!!,
-                        Navigation.Browser.Type.Normal
+                            NavigationSpec.Browser.Type.Normal
                         )
                     is InfoNavigation.Mail -> NavigationSpec.Email(NavigationSpec.Email.EmailSettings(to = action.bundle?.get(
                         MailSpecRow.ToRow) ?: emptyList(), subject = action.bundle?.get(MailSpecRow.SubjectRow)))
