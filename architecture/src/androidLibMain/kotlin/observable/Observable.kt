59bec9a7988faf6d74a074069f06221421f51ab1
Gijs van Veen <gijs@splendo.com>
2021-06-10 13:36:29 +0200

    Make LiveData properties of observable again

---
+++ architecture/src/androidLibMain/kotlin/observable/Observable.kt
@@ -0,0 +1,119 @@
+/*
+ Copyright 2021 Splendo Consulting B.V. The Netherlands
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+@file:JvmName("AndroidObservable")
+
+package com.splendo.kaluga.architecture.observable
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
+import com.splendo.kaluga.architecture.observable.BaseSimpleDisposable
+import com.splendo.kaluga.architecture.observable.BasicSubject
+import com.splendo.kaluga.architecture.observable.Initialized
+import com.splendo.kaluga.architecture.observable.ObservableOptional
+import com.splendo.kaluga.architecture.observable.Uninitialized
+import com.splendo.kaluga.base.utils.EmptyCompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.properties.ReadOnlyProperty
+
+actual interface WithState<T> {
+    actual val stateFlow:StateFlow<T>
+    actual val valueDelegate: ReadOnlyProperty<Any?, T>
+    val liveData: LiveData<T>
+        get()= stateFlow.asLiveData()
+}
+
+val <R:T, T, OO: ObservableOptional<R>> BasicSubject<R, T, OO>.liveDataObserver: Observer<T>
+    get() = Observer<T> { this.post(it) }
+
+private fun <B, R:T, T, OO: ObservableOptional<R>> B.mutableLiveData(): MutableLiveData<R> where B : BasicSubject<R, T, OO>, B : WithMutableState<R> {
+    val mediatorLiveData = MediatorLiveData<R>()
+    mediatorLiveData.addSource(stateFlow.asLiveData()) { value ->
+        mediatorLiveData.postValue(value)
+    }
+
+    return mediatorLiveData
+}
+
+fun <T> LiveData<T>.observeOnCoroutine(coroutineScope: CoroutineScope, observer: Observer<T>) {
+    coroutineScope.launch(Dispatchers.Main.immediate) {
+        observeForever(observer)
+        val neverCompleting = EmptyCompletableDeferred()
+        neverCompleting.await()
+    }.invokeOnCompletion {
+        removeObserver(observer)
+    }
+}
+
+actual abstract class BaseSubject<R:T, T, OO : ObservableOptional<R>> actual constructor(
+    observation: Observation<R, T, OO>,
+    stateFlowToBind:()-> StateFlow<R?>
+): AbstractBaseSubject<R, T, OO>(observation, stateFlowToBind) {
+
+    abstract val mutableLiveData: MutableLiveData<R>
+    abstract fun liveDataObserver(): Observer<R>
+
+    final override fun bind(coroutineScope: CoroutineScope, context: CoroutineContext) {
+        super.bind(coroutineScope, context)
+        mutableLiveData.observeOnCoroutine(coroutineScope, liveDataObserver())
+    }
+}
+
+actual abstract class BaseUninitializedSubject<T> actual constructor(
+    observation: ObservationUninitialized<T>
+): AbstractBaseUninitializedSubject<T>(observation) {
+
+    override val mutableLiveData: MutableLiveData<T> by lazy {
+        val mediatorLiveData = MediatorLiveData<T>()
+        mediatorLiveData.addSource(stateFlow.asLiveData()) { value ->
+            mediatorLiveData.postValue(value)
+        }
+        mediatorLiveData
+    }
+    override fun liveDataObserver() = Observer<T> { value -> value?.let { post(it) } }
+}
+
+actual abstract class BaseInitializedSubject<T> actual constructor(observation: ObservationInitialized<T>) : AbstractBaseInitializedSubject<T>(observation) {
+
+    override val mutableLiveData: MutableLiveData<T> by lazy { this.mutableLiveData() }
+    override fun liveDataObserver() = liveDataObserver
+
+    actual constructor(
+        initialValue: ObservableOptional.Value<T>,
+        coroutineContext: CoroutineContext,
+    ) : this (ObservationInitialized(initialValue, coroutineContext))
+}
+
+actual abstract class BaseDefaultSubject<R:T?, T> actual constructor(
+    observation: ObservationDefault<R, T?>
+) : AbstractBaseDefaultSubject<R, T>(observation) {
+
+    override val mutableLiveData: MutableLiveData<R> = this.mutableLiveData()
+    override fun liveDataObserver() = Observer<R> { post(it) }
+
+    actual constructor(
+        defaultValue: ObservableOptional.Value<R>,
+        initialValue: ObservableOptional.Value<T?>,
+        coroutineContext: CoroutineContext
+    ) : this(observation = ObservationDefault<R, T?>(defaultValue, initialValue, coroutineContext))
+}
--- architecture/src/commonMain/kotlin/observable/Observable.kt
+++ architecture/src/commonMain/kotlin/observable/Observable.kt
@@ -369,7 +369,6 @@ interface InitializedObservable<T>:BasicObservable<T, T, Value<T>>, Initialized<
 interface UninitializedObservable<T>:BasicObservable<T, T, ObservableOptional<T>>, Uninitialized<T>
 
 interface BasicSubject<R:T, T, OO : ObservableOptional<R>>:BasicObservable<R, T, OO>, SuspendableSetter<T> {
-    var boundCoroutine: CoroutineScope?
     fun bind(coroutineScope: CoroutineScope)
     fun bind(coroutineScope: CoroutineScope, context: CoroutineContext)
 }
@@ -382,7 +381,7 @@ interface DefaultObservable<R:T?, T>:BasicObservable<R, T?, Value<R>>, DefaultIn
 
 interface DefaultSubject<R:T?, T>:BasicSubject<R, T?, Value<R>>, DefaultObservable<R, T>, MutableDefaultInitialized<R, T?>
 
-interface WithState<T> {
+expect interface WithState<T> {
     val stateFlow:StateFlow<T>
     val valueDelegate:ReadOnlyProperty<Any?, T>
 }
@@ -459,24 +458,26 @@ abstract class BaseDefaultObservable<R:T?, T>(
 /**
  * A Subject is an [BaseObservable] with a value that can be changed using the [post] method from the [Postable] interface
  */
-abstract class BaseSubject<R:T, T, OO : ObservableOptional<R>>(
+abstract class AbstractBaseSubject<R:T, T, OO : ObservableOptional<R>>(
     observation: Observation<R, T, OO>,
     private val stateFlowToBind:()->StateFlow<R?>)
     : BaseObservable<R, T, OO>(observation), BasicSubject<R, T, OO> {
 
-    final override var boundCoroutine: CoroutineScope? = null
-
     final override fun bind(coroutineScope: CoroutineScope) = bind(coroutineScope, observation.context)
-    final override fun bind(coroutineScope: CoroutineScope, context: CoroutineContext) {
-        boundCoroutine = coroutineScope
+    override fun bind(coroutineScope: CoroutineScope, context: CoroutineContext) {
         coroutineScope.launch(context) {
             stateFlowToBind().collect { set(it as T) }
         }
     }
 }
 
+expect abstract class BaseSubject<R:T, T, OO : ObservableOptional<R>>(
+    observation: Observation<R, T, OO>,
+    stateFlowToBind:()->StateFlow<R?>)
+    : AbstractBaseSubject<R, T, OO>
+
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
-abstract class BaseInitializedSubject<T>(override val observation: ObservationInitialized<T>)
+abstract class AbstractBaseInitializedSubject<T>(override val observation: ObservationInitialized<T>)
     : BaseSubject<T, T, Value<T>>(
         observation,
         { observation.stateFlow }
@@ -493,8 +494,15 @@ abstract class BaseInitializedSubject<T>(override val observation: ObservationIn
         observation.currentObserved
 }
 
+expect abstract class BaseInitializedSubject<T>(observation: ObservationInitialized<T>) : AbstractBaseInitializedSubject<T> {
+    constructor(
+        initialValue: Value<T>,
+        coroutineContext: CoroutineContext = Dispatchers.Main.immediate,
+    )
+}
+
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
-abstract class BaseUninitializedSubject<T>(
+abstract class AbstractBaseUninitializedSubject<T>(
     override val observation: ObservationUninitialized<T>
 ) : BaseSubject<T, T, ObservableOptional<T>> (
     observation,
@@ -506,6 +514,10 @@ abstract class BaseUninitializedSubject<T>(
         observation.observedValue
 
 }
+
+expect abstract class BaseUninitializedSubject<T>(
+    observation: ObservationUninitialized<T>
+): AbstractBaseUninitializedSubject<T>
 
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 abstract class BaseUninitializedObservable<T>(
@@ -556,7 +568,7 @@ class SimpleDefaultSubject<R:T?, T>(
 }
 
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE") // deliberate
-abstract class BaseDefaultSubject<R:T?, T>(
+abstract class AbstractBaseDefaultSubject<R:T?, T>(
     override val observation: ObservationDefault<R, T?>
 ) : BaseSubject<R, T?, Value<R>>(
     observation,
@@ -573,4 +585,15 @@ abstract class BaseDefaultSubject<R:T?, T>(
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): Value<R> =
         observation.currentObserved
+}
+
+expect abstract class BaseDefaultSubject<R:T?, T>(
+    observation: ObservationDefault<R, T?>
+) : AbstractBaseDefaultSubject<R, T> {
+
+    constructor(
+        defaultValue: Value<R>,
+        initialValue: Value<T?>,
+        coroutineContext: CoroutineContext = Dispatchers.Main.immediate
+    )
 }
