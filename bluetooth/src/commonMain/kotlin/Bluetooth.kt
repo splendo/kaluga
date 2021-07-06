ebb44ab707a5f4ec674e24eee28a86840b5d0f97
Grigory Avdyushin <avdyushin.g@gmail.com>
2021-07-06 18:26:50 +0200

    Update Bluetooth.kt

--- bluetooth/src/commonMain/kotlin/Bluetooth.kt
+++ bluetooth/src/commonMain/kotlin/Bluetooth.kt
@@ -40,11 +40,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
