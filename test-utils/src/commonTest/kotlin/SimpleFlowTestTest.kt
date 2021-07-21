f27749f93bbe0af9381d47cebc5fe34493aab082
Tijl Houtbeckers <tijl@houtbecke.rs>
2021-07-21 11:33:44 +0200

    README update for FlowTest with small example

--- test-utils/src/commonTest/kotlin/SimpleFlowTestTest.kt
+++ test-utils/src/commonTest/kotlin/SimpleFlowTestTest.kt
@@ -14,11 +14,48 @@
     limitations under the License.
 
  */
-package com.splendo.kaluga.test
+package com.splendo.kaluga.base.test.flow
 
+import com.splendo.kaluga.base.utils.EmptyCompletableDeferred
+import com.splendo.kaluga.base.utils.complete
+import com.splendo.kaluga.test.SimpleFlowTest
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class SuperSimpleFlowTestTest: SimpleFlowTest<Int>() {
+    override val flow: () -> Flow<Int> = { flowOf(1, 2, 3) }
+
+    @Test
+    // use testWithFlow to run the test
+    fun test() = testWithFlow {
+        val complete = EmptyCompletableDeferred()
+
+        // tests values collected from the flow on the main thread asynchronously
+        test {
+            assertEquals(1, it)
+        }
+
+        test {
+            assertEquals(2, it)
+            delay(10)
+            complete.complete()
+        }
+        // wait for all asynchronous tests to be completed
+        action {
+            assertTrue(complete.isCompleted)
+        }
+        test {
+            assertEquals(3, it)
+        }
+        // no action needed, last test blocks will be run before test is complete
+    }
+}
+
 
 class SimpleFlowTestTest: SimpleFlowTest<Int>() {
 
