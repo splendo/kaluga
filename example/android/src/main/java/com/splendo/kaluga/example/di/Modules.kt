adccfde5a360b4f54e42f1e4a150a57d0f366ac5
Dennis Skokov <dennis@splendo.com>
2021-07-22 17:37:25 +0200

    Fix calendar permission

--- example/android/src/main/java/com/splendo/kaluga/example/di/Modules.kt
+++ example/android/src/main/java/com/splendo/kaluga/example/di/Modules.kt
@@ -72,6 +72,7 @@ import com.splendo.kaluga.permissions.Permission
 import com.splendo.kaluga.permissions.Permissions
 import com.splendo.kaluga.permissions.PermissionsBuilder
 import com.splendo.kaluga.permissions.bluetooth.registerBluetoothPermission
+import com.splendo.kaluga.permissions.calendar.registerCalendarPermission
 import com.splendo.kaluga.permissions.camera.registerCameraPermission
 import com.splendo.kaluga.permissions.contacts.registerContactsPermission
 import com.splendo.kaluga.permissions.location.LocationPermission
@@ -96,7 +97,7 @@ val utilitiesModule = module {
                 registerNotificationsPermission()
                 registerContactsPermission()
                 registerMicrophonePermission()
-                registerCameraPermission()
+                registerCalendarPermission()
             }
         )
     }
--- example/ios/KotlinNativeFramework/src/KotlinNativeFrameworkMain/kotlin/permissions/KNPermissionsFramework.kt
+++ example/ios/KotlinNativeFramework/src/KotlinNativeFrameworkMain/kotlin/permissions/KNPermissionsFramework.kt
@@ -3,6 +3,7 @@ package permissions
 import com.splendo.kaluga.permissions.Permissions
 import com.splendo.kaluga.permissions.PermissionsBuilder
 import com.splendo.kaluga.permissions.bluetooth.registerBluetoothPermission
+import com.splendo.kaluga.permissions.calendar.registerCalendarPermission
 import com.splendo.kaluga.permissions.camera.registerCameraPermission
 import com.splendo.kaluga.permissions.contacts.registerContactsPermission
 import com.splendo.kaluga.permissions.location.registerLocationPermission
@@ -21,7 +22,7 @@ class KNPermissionsFramework {
                 registerNotificationsPermission()
                 registerContactsPermission()
                 registerMicrophonePermission()
-                registerCameraPermission()
+                registerCalendarPermission()
             }
         )
     }
