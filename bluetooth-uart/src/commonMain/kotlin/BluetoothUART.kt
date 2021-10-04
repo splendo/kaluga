30b2231e613e67ec18447e009c87b3230f0b9341
Grigory Avdyushin <avdyushin.g@gmail.com>
2021-10-04 12:48:57 +0200

    Helper functions added

--- bluetooth-uart/src/commonMain/kotlin/BluetoothUART.kt
+++ bluetooth-uart/src/commonMain/kotlin/BluetoothUART.kt
@@ -15,13 +15,14 @@
 
  */
 
+package com.splendo.kaluga.bluetooth.uart
+
 import com.splendo.kaluga.bluetooth.advertisement
 import com.splendo.kaluga.bluetooth.characteristics
 import com.splendo.kaluga.bluetooth.device.Device
 import com.splendo.kaluga.bluetooth.device.DeviceState
 import com.splendo.kaluga.bluetooth.get
 import com.splendo.kaluga.bluetooth.services
-import com.splendo.kaluga.bluetooth.state
 import com.splendo.kaluga.bluetooth.uuidFrom
 import com.splendo.kaluga.logging.debug
 import kotlinx.coroutines.CoroutineScope
@@ -33,29 +34,58 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.launch
 
-interface BluetoothUARTService {
+interface BluetoothUARTInterface {
+    /** Represents data (as [Flow] of [ByteArray]?) received from TX Characteristic */
     val received: Flow<ByteArray?>
+    /** Writes data (as [ByteArray]) to RX Characteristic */
     suspend fun send(data: ByteArray): Boolean
 }
 
+/** Returns [Flow] of [Boolean] set to `true` if [Device] has UART Service */
 fun Flow<Device?>.hasUART() = advertisement()
     .mapLatest {
         it.serviceUUIDs.contains(uuidFrom(BluetoothUART.UART_SERVICE_UUID))
     }
 
+/** Returns [Flow] of UART Service */
 fun Flow<Device?>.uartService() = services()[uuidFrom(BluetoothUART.UART_SERVICE_UUID)]
-fun Flow<Device?>.rxCharacteristic() = uartService().characteristics()[uuidFrom(BluetoothUART.UART_RX_UUID)]
-fun Flow<Device?>.txCharacteristic() = uartService().characteristics()[uuidFrom(BluetoothUART.UART_TX_UUID)]
+/** Returns [Flow] of UART Service */
+fun Flow<DeviceState>.uartService() = transformLatest {
+    emit(
+        when (it) {
+            is DeviceState.Connected.Idle -> it.services.firstOrNull { service ->
+                service.uuid == uuidFrom(BluetoothUART.UART_SERVICE_UUID)
+            }
+            else -> null
+        }
+    )
+}
+/** Returns [Flow] of UART RX Characteristic */
+fun Flow<Device?>.rxCharacteristic() = uartService()
+    .characteristics()[uuidFrom(BluetoothUART.UART_RX_UUID)]
+/** Returns [Flow] of UART RX Characteristic */
+fun Flow<DeviceState>.rxCharacteristic() = uartService()
+    .characteristics()[uuidFrom(BluetoothUART.UART_RX_UUID)]
+/** Returns [Flow] of UART TX Characteristic */
+fun Flow<Device?>.txCharacteristic() = uartService()
+    .characteristics()[uuidFrom(BluetoothUART.UART_TX_UUID)]
+/** Returns [Flow] of UART TX Characteristic */
+fun Flow<DeviceState>.txCharacteristic() = uartService()
+    .characteristics()[uuidFrom(BluetoothUART.UART_TX_UUID)]
 
 class BluetoothUART(
-    private val device: Flow<Device>
-) : BluetoothUARTService {
+    private val deviceStateFlow: Flow<DeviceState>
+) : BluetoothUARTInterface {
 
     companion object {
+        /** UART Service UUID */
         const val UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+        /** UART RX UUID (Write and Write Without Response) */
         const val UART_RX_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+        /** UART TX UUID (Notify) */
         const val UART_TX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
         private const val TAG = "BluetoothUART"
@@ -66,10 +96,10 @@ class BluetoothUART(
 
     init {
         coroutineScope.launch {
-            device.state().collectLatest {
+            deviceStateFlow.collectLatest {
                 when (it) {
-                    is DeviceState.Connected.Idle -> if(!setUpNotifications()) {
-                        debug(TAG, "Can't setup notifications!")
+                    is DeviceState.Connected.Idle -> if (!setupTxNotifications()) {
+                        debug(TAG, "Can't setup Tx notifications!")
                     }
                     else -> {}
                 }
@@ -77,23 +107,20 @@ class BluetoothUART(
         }
     }
 
-    override val received = device
-        .rxCharacteristic()
+    override val received = deviceStateFlow
+        .txCharacteristic()
         .filterNotNull()
         .flatMapLatest { it }
 
-    override suspend fun send(data: ByteArray): Boolean {
-        val tx = device.txCharacteristic().firstOrNull() ?: return false
-        return tx.writeValue(data).completedSuccessfully.await()
+    override suspend fun send(data: ByteArray) = writeRxCharacteristic(data)
+
+    private suspend fun writeRxCharacteristic(data: ByteArray): Boolean {
+        val rx = deviceStateFlow.rxCharacteristic().firstOrNull() ?: return false
+        return rx.writeValue(data).completedSuccessfully.await()
     }
 
-    private suspend fun setUpNotifications(): Boolean {
-        val rx = device.rxCharacteristic().firstOrNull() ?: return false
-        val tx = device.txCharacteristic().firstOrNull() ?: return false
-
-        val rxEnabled = rx.enableNotification()?.completedSuccessfully?.await() ?: true
-        val txEnabled = tx.enableNotification()?.completedSuccessfully?.await() ?: true
-
-        return rxEnabled && txEnabled
+    private suspend fun setupTxNotifications(): Boolean {
+        val tx = deviceStateFlow.txCharacteristic().firstOrNull() ?: return false
+        return tx.enableNotification()?.completedSuccessfully?.await() ?: true
     }
 }
