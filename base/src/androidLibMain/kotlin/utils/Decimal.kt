a51274fcb176c1b457871a6d875107b0d383dfa0
Carmelo Gallo <work@carmelogallo.com>
2021-11-09 17:32:55 +0100

    Fixed precision when converting from decimal (leftover)

--- base/src/androidLibMain/kotlin/utils/Decimal.kt
+++ base/src/androidLibMain/kotlin/utils/Decimal.kt
@@ -106,7 +106,7 @@ actual fun Decimal.round(scale: Int, roundingMode: RoundingMode) =
         )
     )
 
-actual fun Number.toDecimal() = BigDecimal.valueOf(this.toDouble())
+actual fun Number.toDecimal() = BigDecimal(this.toString())
 actual fun String.toDecimal() = BigDecimal(this)
 
 actual fun Decimal.toDouble() = this.toDouble()
