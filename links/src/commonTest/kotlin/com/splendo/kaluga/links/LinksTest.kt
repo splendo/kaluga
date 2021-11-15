ffad347c1eefaa51f6564cf6ac00ed4bb5a97ed5
corrado4eyes <corradoquattrocchi@hotmail.it>
2021-11-15 09:13:57 +0100

    Move dummy properties to this file.

--- links/src/commonTest/kotlin/com/splendo/kaluga/links/LinksTest.kt
+++ links/src/commonTest/kotlin/com/splendo/kaluga/links/LinksTest.kt
@@ -17,11 +17,69 @@
 
 package com.splendo.kaluga.links
 
-import com.splendo.kaluga.links.manager.DataTypesValues
 import com.splendo.kaluga.links.manager.MockLinksBuilder
 import com.splendo.kaluga.links.manager.TestConstants
+import kotlinx.serialization.Serializable
 import kotlin.test.Test
 import kotlin.test.assertEquals
+
+@Serializable
+enum class MyEnum {
+    A, B, C
+}
+
+@Serializable
+data class DataTypesValues(
+    val stringValue: String,
+    val intValue: Int,
+    val longValue: Long,
+    val floatValue: Float,
+    val doubleValue: Double,
+    val booleanValue: Boolean,
+    val enumValue: MyEnum,
+    val listValue: List<String>,
+    val nullableValue: String? = null
+) {
+    companion object {
+        val validParameters = mapOf(
+            "stringValue" to "Test",
+            "intValue" to 0,
+            "longValue" to 3L,
+            "floatValue" to 3.14f,
+            "doubleValue" to 3.14,
+            "booleanValue" to true,
+            "enumValue" to "A",
+            "listValue.0" to "zero",
+            "listValue.1" to "first",
+            "listValue.2" to "second",
+            "nullableValue" to null // can also be excluded from values since has default value.
+        )
+        val expectedValidValues = DataTypesValues(
+            "Test",
+            0,
+            3L,
+            3.14f,
+            3.14,
+            true,
+            MyEnum.A,
+            listOf("zero", "first", "second"),
+            null
+        )
+
+        val url = """https://test.io?
+                stringValue=Test&
+                intValue=0&
+                longValue=3&
+                floatValue=3.14&
+                doubleValue=3.14&
+                booleanValue=true&
+                enumValue=A&
+                listValue.0=zero&
+                listValue.1=first&
+                listValue.2=second&
+                nullableValue""".lines().joinToString("") { it.trim() }
+    }
+}
 
 class LinksTest {
 
