b549cc7c6b3a1ccac576d3c501c41b3898ae4726
Alex <alexandros@splendo.com>
2021-11-23 17:20:36 +0100

    Added more tests

--- scientific/src/commonTest/kotlin/unit/AreaUnitTest.kt
+++ scientific/src/commonTest/kotlin/unit/AreaUnitTest.kt
@@ -17,6 +17,21 @@
 
 package com.splendo.kaluga.scientific.unit
 
+import com.splendo.kaluga.scientific.assertEqualScientificValue
+import com.splendo.kaluga.scientific.converter.energy.div
+import com.splendo.kaluga.scientific.converter.force.div
+import com.splendo.kaluga.scientific.converter.length.times
+import com.splendo.kaluga.scientific.converter.linearMassDensity.div
+import com.splendo.kaluga.scientific.converter.linearMassDensity.times
+import com.splendo.kaluga.scientific.converter.luminousEnergy.div
+import com.splendo.kaluga.scientific.converter.luminousFlux.div
+import com.splendo.kaluga.scientific.converter.luminousIntensity.div
+import com.splendo.kaluga.scientific.converter.magneticFlux.div
+import com.splendo.kaluga.scientific.converter.momentum.div
+import com.splendo.kaluga.scientific.converter.volume.div
+import com.splendo.kaluga.scientific.converter.volumetricFlow.div
+import com.splendo.kaluga.scientific.converter.weight.div
+import com.splendo.kaluga.scientific.invoke
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -34,7 +49,6 @@ class AreaUnitTest {
         assertEquals(1e-6, SquareMeter.convert(1, SquareKilometer))
         assertEquals(0.0001, SquareMeter.convert(1, Hectare))
 
-
         assertEquals(1550.0, SquareMeter.convert(1, SquareInch, 0))
         assertEquals(10.7639, SquareMeter.convert(1, SquareFoot, 4))
         assertEquals(1.19599, SquareMeter.convert(1, SquareYard, 5))
@@ -46,5 +60,89 @@ class AreaUnitTest {
     fun areaDensityConversionTest() {
         assertEquals(0.204816, (Kilogram per SquareMeter).convert(1, Pound per SquareFoot, 6))
         assertEquals(6.022e+26, (Kilogram per SquareMeter).convert(1, Dalton per SquareMeter))
+    }
+
+    @Test
+    fun areaFromEnergyAndSurfaceTensionTest() {
+        assertEquals(1(SquareMeter), 2(Joule) / 2(Newton per Meter))
+        assertEquals(1(SquareFoot), 2(FootPoundForce) / 2(PoundForce per Foot))
+    }
+
+    @Test
+    fun areaFromForceAndPressureTest() {
+        assertEquals(1(SquareMeter), 2(Newton) / 2(Pascal))
+        assertEquals(1(SquareFoot), 2(PoundForce) / 2(PoundSquareFoot))
+    }
+
+    @Test
+    fun areaFromLengthAndWidthTest() {
+        assertEquals(4(SquareMeter), 2(Meter) * 2(Meter))
+        assertEquals(4(SquareFoot), 2(Foot) * 2(Foot))
+    }
+
+    @Test
+    fun areaFromLinearMassDensityAndDensityTest() {
+        assertEquals(1(SquareMeter), 2(Kilogram per Meter) / 2(Kilogram per CubicMeter))
+        assertEquals(1(SquareFoot), 2(Pound per Foot) / 2(Pound per CubicFoot))
+    }
+
+    @Test
+    fun areaFromLinearMassDensityAndSpecificVolumeTest() {
+        assertEquals(4(SquareMeter), 2(Kilogram per Meter) * 2(CubicMeter per Kilogram))
+        assertEquals(4(SquareFoot), 2(Pound per Foot) * 2(CubicFoot per Pound))
+    }
+
+    @Test
+    fun areaFromLuminousEnergyAndExposureTest() {
+        assertEquals(1(SquareMeter), 2(Lumen x Second) / 2(Lux x Second))
+        assertEquals(1(SquareFoot), 2(Lumen x Second) / 2(FootCandle x Second))
+    }
+
+    @Test
+    fun areaFromFluxAndIlluminanceTest() {
+        assertEquals(1(SquareMeter), 2(Lumen) / 2(Lux))
+        assertEquals(1(SquareFoot), 2(Lumen) / 2(FootCandle))
+    }
+
+    @Test
+    fun areaFromLuminousIntensityAndLuminanceTest() {
+        assertEquals(1(SquareMeter), 2(Candela) / 2(Nit))
+        // assertEquals(1(SquareFoot), 2(Candela) / 2(FootLambert)) FIXME I yield 3.141592653589793
+    }
+
+    @Test
+    fun areaFromFluxAndInductionTest() {
+        assertEquals(1(SquareMeter), 2(Weber) / 2(Tesla))
+        // assertEquals(1(SquareFoot), 2(Weber) / 2(Tesla)) FIXME
+    }
+
+    @Test
+    fun areaFromMomentumAndDynamicViscosityTest() {
+        assertEquals(1(SquareMeter), 2(Kilogram x (Meter per Second)) / 2(Pascal x Second))
+        // assertEquals(1(SquareFoot), 2(Pound x (Foot per Second)) / 2(PoundSquareFoot x Second)) FIXME I yield 0.031080950171567253
+    }
+
+    @Test
+    fun areaFromVolumeAndHeightTest() {
+        assertEquals(1(SquareMeter), 2(CubicMeter) / 2(Meter))
+        assertEquals(1(SquareFoot), 2(CubicFoot) / 2(Foot))
+    }
+
+    @Test
+    fun areaFromVolumetricFlowAndVolumetricFluxTest() {
+        assertEqualScientificValue(
+            1(SquareMeter),
+            2(CubicMeter per Second) / 2((CubicMeter per Second) per SquareMeter)
+        )
+        assertEqualScientificValue(
+            1(SquareFoot),
+            2(CubicFoot per Second) / 2((CubicFoot per Second) per SquareFoot)
+        )
+    }
+
+    @Test
+    fun areaFromWeightAndAreaDensityTest() {
+        assertEqualScientificValue(1(SquareMeter), 2(Kilogram) / 2(Kilogram per (SquareMeter)))
+        assertEqualScientificValue(1(SquareFoot), 2(Pound) / 2(Pound per (SquareFoot)))
     }
 }
