c5cb48e265230ac36dd4b099755e8151c8d5e44c
Carmelo Gallo <work@carmelogallo.com>
2021-11-24 11:06:24 +0100

    Added MassFlowRateUnitTest

---
+++ scientific/src/commonTest/kotlin/unit/MassFlowRateUnitTest.kt
@@ -0,0 +1,30 @@
+/*
+ Copyright 2021 Splendo Consulting B.V. The Netherlands
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+
+package com.splendo.kaluga.scientific.unit
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MassFlowRateUnitTest {
+
+    @Test
+    fun massFlowRateConversionTest() {
+        assertEquals(132.28, (Gram per Millisecond).convert(1.0, Pound per Minute, 2))
+        assertEquals(1.12, (ImperialTon per Second).convert(1.0, UsTon per Second))
+    }
+}
