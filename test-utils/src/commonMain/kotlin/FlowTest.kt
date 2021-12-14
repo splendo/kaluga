beb8526b650a5fc7bfe60312931757b4faaa1df6
Carmelo Gallo <work@carmelogallo.com>
2021-12-14 18:02:07 +0100

    Fixed missing import

--- test-utils/src/commonMain/kotlin/FlowTest.kt
+++ test-utils/src/commonMain/kotlin/FlowTest.kt
@@ -34,6 +34,7 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
 import kotlin.native.concurrent.SharedImmutable
 import kotlin.native.concurrent.ThreadLocal
 import kotlin.test.AfterTest
