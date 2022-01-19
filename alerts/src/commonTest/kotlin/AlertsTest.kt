07e62ca1db5e79cf46b0e7f5e37b42f282c97e52
corrado4eyes <corradoquattrocchi@hotmail.it>
2022-01-19 13:31:45 +0100

    Fix tests and add one for color.

--- alerts/src/commonTest/kotlin/AlertsTest.kt
+++ alerts/src/commonTest/kotlin/AlertsTest.kt
@@ -1,10 +1,14 @@
 package com.splendo.kaluga.test
 
 import com.splendo.kaluga.alerts.Alert
+import com.splendo.kaluga.resources.Color
+import com.splendo.kaluga.resources.colorFrom
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
+
+val whiteColor: Color = colorFrom(255, 255, 255)
 
 class AlertsTest {
 
@@ -13,7 +17,7 @@ class AlertsTest {
 
         var handlerWasCalled = false
         val handler = { handlerWasCalled = true }
-        val action = Alert.Action("action", Alert.Action.Style.DEFAULT, handler)
+        val action = Alert.Action("action", Alert.Action.Style.DEFAULT, null, handler)
         val alert = Alert("title", "message", listOf(action))
 
         assertEquals(alert.title, "title")
@@ -21,6 +25,27 @@ class AlertsTest {
         assertEquals(alert.actions.count(), 1)
         assertEquals(alert.actions.first().title, "action")
         assertEquals(alert.actions.first().style, Alert.Action.Style.DEFAULT)
+        assertEquals(alert.actions.first().handler, handler)
+
+        assertFalse(handlerWasCalled)
+        action.handler()
+        assertTrue(handlerWasCalled)
+    }
+
+    @Test
+    fun testAlertInitializationWithColor() {
+
+        var handlerWasCalled = false
+        val handler = { handlerWasCalled = true }
+        val action = Alert.Action("action", Alert.Action.Style.DEFAULT, whiteColor, handler)
+        val alert = Alert("title", "message", listOf(action))
+
+        assertEquals(alert.title, "title")
+        assertEquals(alert.message, "message")
+        assertEquals(alert.actions.count(), 1)
+        assertEquals(alert.actions.first().title, "action")
+        assertEquals(alert.actions.first().style, Alert.Action.Style.DEFAULT)
+        assertEquals(alert.actions.first().color, whiteColor)
         assertEquals(alert.actions.first().handler, handler)
 
         assertFalse(handlerWasCalled)
