713feff8ce0fd80df4350d76b20e948c6db61180
Jakub Tomanik <j.tomanik@gmail.com>
2022-01-24 16:21:05 +0100

    Add converters for Dimensionless Units

---
+++ scientific/src/commonMain/kotlin/converter/decimal/convertFromDimensionless.kt
@@ -0,0 +1,41 @@
+/*
+ Copyright 2022 Splendo Consulting B.V. The Netherlands
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+
+package com.splendo.kaluga.scientific.converter.decimal
+
+import com.splendo.kaluga.base.utils.Decimal
+import com.splendo.kaluga.base.utils.toDecimal
+import com.splendo.kaluga.scientific.PhysicalQuantity
+import com.splendo.kaluga.scientific.ScientificValue
+import com.splendo.kaluga.scientific.div
+import com.splendo.kaluga.scientific.invoke
+import com.splendo.kaluga.scientific.times
+import com.splendo.kaluga.scientific.unit.One
+import com.splendo.kaluga.scientific.unit.ScientificUnit
+import com.splendo.kaluga.scientific.unit.constant
+
+infix operator fun Decimal.div(modifier: ScientificValue<PhysicalQuantity.Dimensionless, *>): Decimal {
+    val scalar = this.invoke(One)
+    val result = scalar / modifier
+    return result.value.toDecimal()
+}
+
+infix operator fun Decimal.times(modifier: ScientificValue<PhysicalQuantity.Dimensionless, *>): Decimal {
+    val scalar = this.invoke(One)
+    val result = scalar * modifier
+    return result.value.toDecimal()
+}
