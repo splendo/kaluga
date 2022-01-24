2fb284b6d3cac1c3768ce9871cda8a8a966e439c
corrado4eyes <corradoquattrocchi@hotmail.it>
2022-01-24 10:04:59 +0100

    Remove AlertsTestUtils.

--- alerts/src/commonTest/kotlin/AlertsTest.kt
+++ alerts/src/commonTest/kotlin/AlertsTest.kt
@@ -1,18 +1,11 @@
 package com.splendo.kaluga.test
 
 import com.splendo.kaluga.alerts.Alert
-import com.splendo.kaluga.resources.Color
 import com.splendo.kaluga.resources.colorFrom
-import kotlin.native.concurrent.ThreadLocal
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
-
-@ThreadLocal
-object AlertsTestUtils {
-    val whiteColor: Color = colorFrom(255, 255, 255)
-}
 
 class AlertsTest {
 
@@ -41,7 +34,7 @@ class AlertsTest {
 
         var handlerWasCalled = false
         val handler = { handlerWasCalled = true }
-        val action = Alert.Action("action", Alert.Action.Style.DEFAULT, AlertsTestUtils.whiteColor, handler)
+        val action = Alert.Action("action", Alert.Action.Style.DEFAULT, colorFrom(255, 255, 255), handler)
         val alert = Alert("title", "message", listOf(action))
 
         assertEquals(alert.title, "title")
@@ -49,7 +42,7 @@ class AlertsTest {
         assertEquals(alert.actions.count(), 1)
         assertEquals(alert.actions.first().title, "action")
         assertEquals(alert.actions.first().style, Alert.Action.Style.DEFAULT)
-        assertEquals(alert.actions.first().textColor, AlertsTestUtils.whiteColor)
+        assertEquals(alert.actions.first().textColor, colorFrom(255, 255, 255))
         assertEquals(alert.actions.first().handler, handler)
 
         assertFalse(handlerWasCalled)
