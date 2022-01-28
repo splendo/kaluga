0a4ce13abaae2ec27408c9b5becba76aec6aefa0
Dennis Skokov <dennis@splendo.com>
2022-01-28 14:40:10 +0100

    Formatter returns empty strig (TMP)

---
+++ scientific/src/commonTest/kotlin/forrmatter/DefaultFormatterTest.kt
@@ -0,0 +1,42 @@
+/*
+ Copyright 2022 Splendo Consulting B.V. The Netherlands
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+
+package com.splendo.kaluga.scientific.forrmatter
+
+import com.splendo.kaluga.scientific.formatter.DefaultFormatter
+import com.splendo.kaluga.scientific.formatter.Formatter
+import com.splendo.kaluga.scientific.invoke
+import com.splendo.kaluga.scientific.unit.Meter
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+class DefaultFormatterTest {
+    lateinit var formatter: Formatter
+
+    @BeforeTest
+    fun setUp() {
+        formatter = DefaultFormatter()
+    }
+
+    @Test
+    fun format__it_converts_scientific_value_to_string() {
+        val value = 1(Meter)
+
+        assertIs<String>(formatter.format(value), "It should convert scientific value to string")
+    }
+}
