1d44c67a6c28b5ef74f6b02c3b208b85ceb97183
Alex <alexandros@splendo.com>
2022-03-10 13:10:48 +0100

    Added last seen to the timeout calculation as BL module usually reports a device long after it is out of range.

--- beacons/src/commonMain/kotlin/Beacons.kt
+++ beacons/src/commonMain/kotlin/Beacons.kt
@@ -46,7 +46,9 @@ class Beacons(
     private val timeoutMs: Long = 10_000,
 ) {
 
-    private companion object { const val TAG = "Beacons" }
+    private companion object {
+        const val TAG = "Beacons"
+    }
 
     private val cache = BeaconsMap()
     private val cacheJob = Job()
@@ -95,7 +97,7 @@ class Beacons(
             }
             cache[beacon.beaconID] = beacon to coroutineScope.launch {
                 debug(TAG, "[Added] $beacon")
-                delay(timeoutMs)
+                delay(beacon.lastSeen.millisecondSinceEpoch + timeoutMs - Date.now().millisecondSinceEpoch)
                 debug(TAG, "[Lost] $beacon")
                 cache.remove(beacon.beaconID)
                 updateList()
@@ -112,7 +114,8 @@ class Beacons(
         this.map(String::lowercase).contains(element.lowercase())
 
     private suspend fun createBeaconWith(device: Device): BeaconInfo? {
-        val serviceData = device.map { it.advertisementData.serviceData }.firstOrNull() ?: return null
+        val serviceData =
+            device.map { it.advertisementData.serviceData }.firstOrNull() ?: return null
         val data = serviceData[Eddystone.SERVICE_UUID] ?: return null
         val frame = Eddystone.unpack(data) ?: return null
         val rssi = device.map { it.rssi }.firstOrNull() ?: 0
@@ -124,3 +127,113 @@ class Beacons(
 operator fun Flow<Set<BeaconInfo>>.get(identifier: Identifier) = map { beacons ->
     beacons.firstOrNull { it.identifier == identifier }
 }
+
+/*
+ Copyright 2020 Splendo Consulting B.V. The Netherlands
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ *//*
+
+
+package com.splendo.kaluga.bluetooth.beacons
+
+import com.splendo.kaluga.base.AtomicReferenceDelegate
+import com.splendo.kaluga.base.utils.Date
+import com.splendo.kaluga.bluetooth.BluetoothService
+import com.splendo.kaluga.bluetooth.device.Device
+import com.splendo.kaluga.bluetooth.device.Identifier
+import com.splendo.kaluga.logging.debug
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+class Beacons(
+    private val bluetooth: BluetoothService,
+    private val timeoutMs: Long = 10_000,
+) {
+
+    private companion object {
+        const val TAG = "Beacons"
+    }
+
+    private var monitoringJob: Job? by AtomicReferenceDelegate()
+
+    private val _beacons = MutableStateFlow(emptyList<BeaconInfo>())
+    val beacons: StateFlow<List<BeaconInfo>> = _beacons.asStateFlow()
+
+    fun startMonitoring(coroutineScope: CoroutineScope) {
+        debug(TAG, "Start monitoring")
+        _beacons.value = emptyList()
+        bluetooth.startScanning()
+        monitoringJob = coroutineScope.launch {
+            launch {
+                bluetooth.devices().collectLatest { list ->
+                    debug(TAG, "Total Bluetooth devices discovered: ${list.size}")
+                    launch { }
+                    updateBeacons(list.mapNotNull { createBeaconWith(it) })
+                }
+            }
+        }
+    }
+
+    fun stopMonitoring() {
+        debug(TAG, "Stop monitoring")
+        bluetooth.stopScanning()
+        monitoringJob?.cancel()
+        monitoringJob = null
+        _beacons.value = emptyList()
+    }
+
+    suspend fun isMonitoring() = bluetooth.isScanning()
+
+    fun isAnyInRange(beaconIds: List<String>) = beacons.map { list ->
+        list.any { beaconIds.containsLowerCased(it.beaconID.asString()) }
+    }
+
+    private suspend fun updateBeacons(discovered: List<BeaconInfo>) {
+        debug(TAG, "Total Beacons discovered: ${discovered.size}")
+        val currentBeacons = _beacons.value.toMutableList()
+        val updatedBeacons = discovered.filter { it.beaconID in currentBeacons.map { it.beaconID } }
+        val newBeacons = discovered.filter { it.beaconID !in currentBeacons.map { it.beaconID } }
+        currentBeacons.removeAll { it.beaconID in updatedBeacons.map { it.beaconID } }
+        currentBeacons.addAll(updatedBeacons + newBeacons)
+        _beacons.value =
+            currentBeacons.filter { it.lastSeen.millisecondSinceEpoch > (Date.now(-timeoutMs).millisecondSinceEpoch) }
+    }
+
+    private fun List<String>.containsLowerCased(element: String): Boolean =
+        this.map(String::lowercase).contains(element.lowercase())
+
+    private suspend fun createBeaconWith(device: Device): BeaconInfo? {
+        val serviceData =
+            device.map { it.advertisementData.serviceData }.firstOrNull() ?: return null
+        val data = serviceData[Eddystone.SERVICE_UUID] ?: return null
+        val frame = Eddystone.unpack(data) ?: return null
+        val rssi = device.map { it.rssi }.firstOrNull() ?: 0
+        val lastSeen = device.map { it.updatedAt }.firstOrNull() ?: Date.now()
+        return BeaconInfo(device.identifier, frame.uid, frame.txPower, rssi, lastSeen)
+    }
+}
+
+operator fun Flow<Set<BeaconInfo>>.get(identifier: Identifier) = map { beacons ->
+    beacons.firstOrNull { it.identifier == identifier }
+}
+*/
