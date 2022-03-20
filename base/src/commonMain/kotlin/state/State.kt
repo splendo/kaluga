976542ad2ef4e629f464d8307ea0837a713a8994
Gijs van Veen <gijs@splendo.com>
2022-03-20 12:13:28 +0100

    Fix ColdStateFlow initialization
    By enforcing calling initialValue

--- base/src/commonMain/kotlin/state/State.kt
+++ base/src/commonMain/kotlin/state/State.kt
@@ -384,6 +384,7 @@ abstract class BaseColdStateRepo<S : State, F : MutableSharedFlow<S>>(
             val isInitialized = lazyMutableFlow.isInitialized()
             val flow = lazyMutableFlow.value
             if (!isInitialized && initialized.compareAndSet(expected = false, new = true)) {
+                debug("ColdStateRepo", "Initialize Flow: $flow for ${this::class.simpleName}")
                 launch(coroutineContext) {
                     flow.onCollectionEvent { event ->
                         when (event) {
@@ -414,9 +415,9 @@ abstract class BaseColdStateRepo<S : State, F : MutableSharedFlow<S>>(
  */
 open class ColdStateFlowRepo<S : State>(
     coroutineContext: CoroutineContext = Dispatchers.Main.immediate,
-    val initChangeStateWithRepo: suspend (S?, ColdStateFlowRepo<S>) -> (suspend () -> S),
+    val initChangeStateWithRepo: suspend (S, ColdStateFlowRepo<S>) -> (suspend () -> S),
     val deinitChangeStateWithRepo: suspend (S, ColdStateFlowRepo<S>) -> (suspend () -> S)?,
-    val firstState: (suspend() -> S)? = null
+    val firstState: () -> S
 ) : StateFlowRepo<S>,
     BaseColdStateRepo<S, MutableStateFlow<S>>(
         context = coroutineContext,
@@ -425,12 +426,12 @@ open class ColdStateFlowRepo<S : State>(
     constructor(
         coroutineContext: CoroutineContext = Dispatchers.Main.immediate,
         // order is different than below because here firstState is mandatory, and to avoid JVM signature clashes
-        firstState: suspend() -> S,
+        firstState: () -> S,
         initChangeState: suspend (S) -> (suspend () -> S),
         deinitChangeState: suspend (S) -> (suspend () -> S)
     ) : this(
         coroutineContext,
-        initChangeStateWithRepo = { state, _ -> state?.let { initChangeState(state) } ?: firstState },
+        initChangeStateWithRepo = { state, _ -> initChangeState(state) },
         deinitChangeStateWithRepo = { state, _ -> deinitChangeState(state) },
         firstState = firstState
     )
@@ -439,7 +440,7 @@ open class ColdStateFlowRepo<S : State>(
         coroutineContext: CoroutineContext = Dispatchers.Main.immediate,
         init: suspend (ColdStateFlowRepo<S>) -> S,
         deinit: suspend (ColdStateFlowRepo<S>) -> S?,
-        firstState: (suspend() -> S)? = null
+        firstState: () -> S
     ) : this(
         coroutineContext,
         initChangeStateWithRepo = { _, repo -> { init(repo) } },
@@ -459,11 +460,9 @@ open class ColdStateFlowRepo<S : State>(
 
     override val lazyMutableFlow: Lazy<MutableStateFlow<S>> =
         lazy {
-            runBlocking {
-                MutableStateFlow(
-                    firstState?.invoke() ?: initChangeStateWithRepo(null, this@ColdStateFlowRepo)()
-                )
-            }
+            MutableStateFlow(
+                firstState()
+            )
         }
 
     override suspend fun firstCollection() {
