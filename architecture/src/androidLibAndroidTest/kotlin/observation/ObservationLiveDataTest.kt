a217c128f1894bc73b749a08bbac5964d2b7ac9c
Gijs van Veen <gijs@splendo.com>
2022-05-24 11:50:20 +0200

    Broken AndroidTest

--- architecture/src/androidLibAndroidTest/kotlin/observation/ObservationLiveDataTest.kt
+++ architecture/src/androidLibAndroidTest/kotlin/observation/ObservationLiveDataTest.kt
@@ -26,7 +26,7 @@ import com.splendo.kaluga.architecture.observable.toInitializedSubject
 import com.splendo.kaluga.base.utils.EmptyCompletableDeferred
 import com.splendo.kaluga.base.utils.complete
 import com.splendo.kaluga.test.base.BaseTest
-import com.splendo.kaluga.test.testBlockingAndCancelScope
+import com.splendo.kaluga.test.base.testBlockingAndCancelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
