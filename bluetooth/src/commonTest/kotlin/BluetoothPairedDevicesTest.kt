0fb0d127278578472715f2d46382e387479e2fa1
Grigory Avdyushin <avdyushin.g@gmail.com>
2022-05-30 14:00:43 +0200

    Create BluetoothPairedDevicesTest.kt

---
+++ bluetooth/src/commonTest/kotlin/BluetoothPairedDevicesTest.kt
@@ -0,0 +1,60 @@
+/*
+ Copyright 2021 Splendo Consulting B.V. The Netherlands
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+
+package com.splendo.kaluga.bluetooth
+
+import co.touchlab.stately.ensureNeverFrozen
+import com.splendo.kaluga.bluetooth.device.randomIdentifier
+import com.splendo.kaluga.bluetooth.scanner.ScanningState
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertIs
+import kotlin.test.assertTrue
+
+class BluetoothPairedDevicesTest : BluetoothFlowTest<ScanningState>() {
+
+    init {
+        ensureNeverFrozen()
+    }
+
+    override val flow = suspend {
+        setup(Setup.BLUETOOTH)
+        bluetooth.scanningStateRepo.stateFlow
+    }
+
+    @Test
+    fun testPairedDevices() = testWithFlow {
+        ensureNeverFrozen()
+
+        test {
+            assertIs<ScanningState.NotInitialized>(it)
+        }
+
+        action {
+            // Trigger initialization
+            assertTrue(bluetooth.pairedDevices().isEmpty())
+        }
+
+        val list =  listOf(randomIdentifier())
+        mockBaseScanner().pairedDevices.value = list
+
+        test {
+            assertIs<ScanningState.Initialized.Enabled>(it)
+            assertContentEquals(list, it.pairedDevices(emptySet()))
+        }
+    }
+}
