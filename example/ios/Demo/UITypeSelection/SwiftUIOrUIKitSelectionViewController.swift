fb9619bc3aaba8e199b51f4ff306ec7a30f487e5
Gijs van Veen <gijs@splendo.com>
2022-12-01 10:00:07 +0100

    Adding SwiftUI views

---
+++ example/ios/Demo/UITypeSelection/SwiftUIOrUIKitSelectionViewController.swift
@@ -0,0 +1,87 @@
+//
+//  Copyright 2022 Splendo Consulting B.V. The Netherlands
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
+import UIKit
+import SwiftUI
+import KalugaExampleShared
+
+class SwiftUIOrUIKitSelectionViewController : UITableViewController {
+
+    struct Const {
+        static let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        static let storyboardId = "SwiftOrUIKit"
+    }
+
+    static func create() {
+
+    }
+
+    var viewModel: SwiftUIOrUIKitSelectionViewModel!
+    private var lifecycleManager: LifecycleManager!
+
+    private var uiTypes = [String]()
+    private var onSelected: ((Int) -> Void)? = nil
+
+    deinit {
+        lifecycleManager.unbind()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        lifecycleManager = viewModel.addLifecycleManager(parent: self) { [weak self] in
+            guard let viewModel = self?.viewModel else { return [] }
+            return [
+                viewModel.uiTypes.observeInitialized { next in
+                    let uiTypes = next ?? []
+                    self?.uiTypes = uiTypes.map { ($0 as! UIType).title }
+                    self?.onSelected = { (index: Int) in viewModel.onUITypePressed(uiType: uiTypes[index] as! UIType) }
+                    self?.tableView.reloadData()
+                }
+            ]
+        }
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return uiTypes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITypesListCell.Const.identifier, for: indexPath) as! UITypesListCell
+        cell.label.text = uiTypes[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let _ = onSelected?(indexPath.row)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
+class UITypesListCell : UITableViewCell {
+
+    struct Const {
+        static let identifier = "ResourcesListCell"
+    }
+
+    @IBOutlet weak var label: UILabel!
+
+}
