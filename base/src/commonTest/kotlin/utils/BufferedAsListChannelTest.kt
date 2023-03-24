a4450bf999cbe2e803d8f05397d79bba99945016
Gijs van Veen <gijs@splendo.com>
2023-03-24 14:39:30 +0100

    "Fix" buffered as list tests

--- base/src/commonTest/kotlin/utils/BufferedAsListChannelTest.kt
+++ base/src/commonTest/kotlin/utils/BufferedAsListChannelTest.kt
@@ -36,13 +36,13 @@ import kotlin.time.Duration.Companion.milliseconds
 class BufferedAsListChannelTest {
 
     @Test
-    fun testEqualChannel() = testBufferedAsListChannel(10.milliseconds, 10.milliseconds, 50..100)
+    fun testEqualChannel() = testBufferedAsListChannel(10.milliseconds, 10.milliseconds, 1..100)
 
     @Test
-    fun testSlowToProduceChannel() = testBufferedAsListChannel(10.milliseconds, 1.milliseconds, 50..100)
+    fun testSlowToProduceChannel() = testBufferedAsListChannel(10.milliseconds, 1.milliseconds, 1..100)
 
     @Test
-    fun testSlowToConsumeChannel() = testBufferedAsListChannel(1.milliseconds, 10.milliseconds, 5..20)
+    fun testSlowToConsumeChannel() = testBufferedAsListChannel(1.milliseconds, 10.milliseconds, 1..100)
 
     @Test
     fun testCancel() = runBlocking {
