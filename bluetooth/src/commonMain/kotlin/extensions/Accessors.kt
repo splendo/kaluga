5af44058f7235b76ddec2f1b26e1e6d989cda7b6
Gijs van Veen <gijs@splendo.com>
2024-03-18 16:15:29 +0100

    Suggestions for improvements

--- bluetooth/src/commonMain/kotlin/extensions/Accessors.kt
+++ bluetooth/src/commonMain/kotlin/extensions/Accessors.kt
@@ -1,23 +1,73 @@
 package com.splendo.kaluga.bluetooth.extensions
 
-import com.splendo.kaluga.base.utils.bytesOf
 import com.splendo.kaluga.bluetooth.Characteristic
+import com.splendo.kaluga.bluetooth.Descriptor
 import com.splendo.kaluga.bluetooth.UUIDException
 import com.splendo.kaluga.bluetooth.characteristics
+import com.splendo.kaluga.bluetooth.descriptors
 import com.splendo.kaluga.bluetooth.device.Device
 import com.splendo.kaluga.bluetooth.services
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 /**
- * Provides access to device data flow by service and characteristic string uuids.
+ * Provides access to device data flow by service and characteristic string uuids and automatically subscribes to updates.
+ * @param serviceUUID string service uuid representation
+ * @param characteristicUUID string characteristic uuid representation
+ * @param coroutineScope the [CoroutineScope] on which to subscribe to any updates to the characteristic
+ * @throws UUIDException.InvalidFormat
+ */
+fun Flow<Device?>.notifyingDataFlow(serviceUUID: String, characteristicUUID: String, coroutineScope: CoroutineScope): SharedFlow<ByteArray> {
+    val dataFlow = dataFlow(serviceUUID, characteristicUUID)
+    var job: Job? = null
+    val shared = MutableSharedFlow<ByteArray>()
+    coroutineScope.launch {
+        shared.subscriptionCount.map {it > 0 }.distinctUntilChanged().collect { isCollecting ->
+            job?.cancel()
+            val characteristic = characteristic(serviceUUID, characteristicUUID)
+            if (isCollecting) {
+                characteristic.enableNotification()
+                job = coroutineScope.launch {
+                    shared.emitAll(dataFlow)
+                }
+            } else {
+                characteristic.disableNotification()
+            }
+        }
+    }
+    return shared.asSharedFlow()
+}
+
+/**
+ * Provides access to device data flow by service, characteristic and descriptor string uuids.
  * @param serviceUUID string service uuid representation
  * @param characteristicUUID string characteristic uuid representation
  * @throws UUIDException.InvalidFormat
  */
-fun Flow<Device?>.dataFlow(serviceUUID: String, characteristicUUID: String) = characteristicsFlow(serviceUUID, characteristicUUID).flatMapConcat {
-    it.enableNotification()
-    it
-}.map { it ?: bytesOf() }
+fun Flow<Device?>.dataFlow(serviceUUID: String, characteristicUUID: String) = characteristicsFlow(serviceUUID, characteristicUUID).flatMapLatest { characteristic ->
+    characteristic.map { it ?: byteArrayOf() }
+}
+
+/**
+ * Provides access to device data flow by service, characteristic and descriptor string uuids.
+ * @param serviceUUID string service uuid representation
+ * @param characteristicUUID string characteristic uuid representation
+ * @throws UUIDException.InvalidFormat
+ */
+fun Flow<Device?>.dataFlow(serviceUUID: String, characteristicUUID: String, descriptorUUID: String) = descriptorsFlow(serviceUUID, characteristicUUID, descriptorUUID).flatMapLatest { descriptor ->
+    descriptor.map { it ?: byteArrayOf() }
+}
 
 /**
  * Provides access to characteristic's flow by service and characteristic string uuids.
@@ -38,5 +88,31 @@ fun Flow<Device?>.characteristicsFlow(serviceUUID: String, characteristicUUID: S
  */
 suspend fun Flow<Device?>.characteristic(serviceUUID: String, characteristicUUID: String) = services()[serviceUUID]
     .characteristics()[characteristicUUID]
+    .filterNotNull()
+    .first()
+
+/**
+ * Provides access to descriptors's flow by service, characteristic and descriptor string uuids.
+ * @param serviceUUID string service uuid representation
+ * @param characteristicUUID string characteristic uuid representation
+ * @param des
+ * @throws UUIDException.InvalidFormat
+ */
+fun Flow<Device?>.descriptorsFlow(serviceUUID: String, characteristicUUID: String, descriptorUUID: String) = services()[serviceUUID]
+    .characteristics()[characteristicUUID]
+    .descriptors()[descriptorUUID]
+    .filterNotNull()
+
+/**
+ * Provides access to [Descriptor] by service, characteristic and descriptor string uuids.
+ * The method will suspend if descriptor is not available.
+ * @param serviceUUID string service uuid representation
+ * @param characteristicUUID string characteristic uuid representation
+ * @param descriptorUUID string descriptor uuid representation
+ * @throws UUIDException.InvalidFormat
+ */
+suspend fun Flow<Device?>.descriptor(serviceUUID: String, characteristicUUID: String, descriptorUUID: String) = services()[serviceUUID]
+    .characteristics()[characteristicUUID]
+        .descriptors()[descriptorUUID]
     .filterNotNull()
     .first()
---
+++ bluetooth/src/commonMain/kotlin/extensions/Attribute.kt
@@ -0,0 +1,18 @@
+package com.splendo.kaluga.bluetooth.extensions
+
+import com.splendo.kaluga.bluetooth.Attribute
+import com.splendo.kaluga.bluetooth.UUIDException
+import com.splendo.kaluga.bluetooth.device.DeviceAction
+import com.splendo.kaluga.bluetooth.get
+import com.splendo.kaluga.bluetooth.uuidFrom
+import kotlinx.coroutines.flow.Flow
+import kotlin.jvm.JvmName
+
+/**
+ * Provides access to an [Attribute] by given string uuid
+ * @param uuidString string attribute uuid representation
+ * @throws UUIDException.InvalidFormat
+ */
+@JvmName("getAttribute")
+operator fun <AttributeType, ReadAction, WriteAction> Flow<List<AttributeType>>.get(uuidString: String) : Flow<AttributeType?>
+where AttributeType : Attribute<ReadAction, WriteAction>, ReadAction : DeviceAction.Read, WriteAction : DeviceAction.Write = this[uuidFrom(uuidString)]
