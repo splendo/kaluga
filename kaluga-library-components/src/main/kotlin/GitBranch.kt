922132d316ccf9072531a881f182fbfcd2d28cac
Gijs van Veen <gijs@splendo.com>
2024-06-10 09:53:54 +0200

    Temp hardcode master branch

--- kaluga-library-components/src/main/kotlin/GitBranch.kt
+++ kaluga-library-components/src/main/kotlin/GitBranch.kt
@@ -16,42 +16,9 @@
  */
 
 import org.gradle.api.Project
-import java.util.Locale
 
 data class GitBranch(val branch: String, val kalugaBranchPostfix: String)
 
 val Project.GitBranch: GitBranch get() {
-    val GITHUB_GIT_BRANCH = System.getenv("GITHUB_REF_NAME") // could also be a tag name
-    val kaluga_branch = System.getProperty("kaluga_branch")
-    val MAVEN_CENTRAL_RELEASE = System.getenv("MAVEN_CENTRAL_RELEASE")
-    val release = MAVEN_CENTRAL_RELEASE?.lowercase(Locale.ENGLISH)?.trim() == "true"
-    val branchFromGit = run {
-        try {
-            val process = ProcessBuilder().command("git rev-parse --abbrev-ref HEAD".split(" ")).start()
-            process.inputStream.bufferedReader().readText()
-        } catch (e: Exception) {
-            logger.info("Unable to determine current branch through git CLI: ${e.message}")
-            "unknown"
-        }
-    }
-
-    // favour user definition of kaluga_branch (if present), otherwise take it from GIT branch:
-    // - if running on CI: favour github's branch detection
-    // - else: try to get it via the `git` CLI.
-    val branch = (kaluga_branch ?: GITHUB_GIT_BRANCH ?: branchFromGit).replace('/', '-').trim().lowercase(Locale.ENGLISH).also {
-        if (it == "HEAD") {
-            logger.warn("Unable to determine current branch: Project is checked out with detached head!")
-        }
-    }
-
-    val kalugaBranchPostfix = (
-        when (branch) {
-            "master", "main", "develop" -> ""
-            else -> "-" + branch
-        } + if (!release) "-SNAPSHOT" else ""
-        ).also {
-            logger.lifecycle("decided branch: '$branch' to postfix '$it', isRelease: $release (from: GITHUB_GIT_BRANCH env: $GITHUB_GIT_BRANCH, kaluga_branch property: $kaluga_branch , MAVEN_CENTRAL_RELEASE env: $MAVEN_CENTRAL_RELEASE , git cli: $branchFromGit)")
-        }
-
-    return GitBranch(branch, "")
+    return GitBranch("master", "")
 }
