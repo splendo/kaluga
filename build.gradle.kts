de31d8084857b347ea4b23a6366b7b4dcc11b5dd
Tijl <tijl@houtbecke.rs>
2024-10-12 00:08:17 +0200

    add version catalog publishing #781

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,25 @@
 plugins {
     id("com.splendo.kaluga.plugin")
     id("rs.houtbecke.gradle.recorder.plugin")
+    `version-catalog`
+    `maven-publish`
 }
 
 apply(from = "gradle/newModule.gradle.kts")
 apply(from = "gradle/copyReports.gradle.kts")
+
+catalog {
+    versionCatalog {
+        library("catalog", "com.splendo.kaluga:catalog:"+libs.versions.kaluga.get())
+        from(files("gradle/libs.versions.toml"))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("catalog") {
+            from(components["versionCatalog"])
+            artifactId = "catalog"
+        }
+    }
+}
