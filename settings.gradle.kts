d576888003ede6661af8412db3fc012ac85c60a7
Tijl Houtbeckers <tijl@houtbecke.rs>
2026-04-02 10:39:34 +0200

    Add foojay toolchain resolver, daemon JVM config, and catalog entries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,10 @@ pluginManagement {
     includeBuild("kaluga-library-components")
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "Kaluga"
 
 /* REMINDER (see header), files should be kept up to date with Gradle and the [.git/workflows] */
